5e58540a74de0ea337a665cf9bffae506cad8c1c
Benjamin6282 <benjamin.cobb@maine.edu>
2025-04-01 19:06:14 -0400

    Sign up button added

--- taskii/lib/main.dart
+++ taskii/lib/main.dart
@@ -231,7 +231,8 @@ class SignUpPage extends StatelessWidget {
                 ),
               ),
               
-              // Login Form
+              // Sign Up Form
+              //email box
               const SizedBox(height: 48),
               TextField(
                 decoration: InputDecoration(
@@ -251,7 +252,56 @@ class SignUpPage extends StatelessWidget {
                   ),
                 ),
               ),
-          
+
+            //password box
+            const SizedBox(height: 16),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Password',
+                hintStyle: const TextStyle(
+                  color: Color(0xFFADAEBC),
+                  fontSize: 16,
+                  fontFamily: 'Inter',
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Color(0xFFE5E5E5)),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Color(0xFFE5E5E5)),
+                ),
+              ),
+            ),
+
+
+            //signup button
+            const SizedBox(height: 16),
+            SizedBox(
+              width: double.infinity,
+              child: TextButton(
+                onPressed: () {
+                  print('Sign Up button pressed');
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: const Color(0xFF171717),
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                child: const Text(
+                  'Sign In',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ),
             ],
           ),
         ),
