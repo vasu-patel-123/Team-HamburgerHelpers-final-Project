f5901d61700e726fea36c22f1915664f2b618aa3
Vasu patel <69396812+vasu-patel-123@users.noreply.github.com>
2025-04-01 18:19:28 -0400

    Create forgotpassword.dart
    Started the forgotpassword page

---
+++ taskii/lib/forgotpassword.dart
@@ -0,0 +1,9 @@
+// Document consists of the forgot password page
+// This page is used to reset the password of the user
+// The user will enter their email address and a password reset link will be sent to their email
+// The user can then click on the link to reset their password
+// The user will be redirected to the reset password page
+// The user can then enter a new password and confirm the new password
+// The user can then click on the reset password button to reset their password
+// The user will be redirected to the sign in page
+// The user can then sign in with their new password
