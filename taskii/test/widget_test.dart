baffb8f5b77fe8a90ba3b9a86b84df1a6cfa93d0
0xG <114037840+0xRum@users.noreply.github.com>
2025-04-01 16:11:31 -0400

    Update widget_test.dart
    updated widget test to ensure login page is shown and verifying the app icon is present in the emulator

--- taskii/test/widget_test.dart
+++ taskii/test/widget_test.dart
@@ -11,20 +11,14 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:taskii/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('App shows login page', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const FigmaToCodeApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that the login page is shown
+    expect(find.byType(LoginPageSignUp), findsOneWidget);
+    
+    // Verify that the app icon is present
+    expect(find.byIcon(Icons.assignment_outlined), findsOneWidget);
   });
 }
