f4598c69aecf001d5c31d103ebaacf198778b71b
Jaron Durkee <61402697+Blarnix@users.noreply.github.com>
2025-04-15 13:33:59 -0400

    Updated naming convention to suit Dart

--- taskii/lib/main.dart
+++ taskii/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'signUp.dart';
+import 'signup.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
