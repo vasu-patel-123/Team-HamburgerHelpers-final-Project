b27782a4b3a7661c68a933bcec454e5429381296
Benjamin6282 <benjamin.cobb@maine.edu>
2025-05-01 21:31:13 -0400

    created forgot password file

--- taskii/lib/forgotpassword.dart
+++ taskii/lib/forgotpassword.dart
@@ -1,9 +1,51 @@
-// Document consists of the forgot password page
-// This page is used to reset the password of the user
-// The user will enter their email address and a password reset link will be sent to their email
-// The user can then click on the link to reset their password
-// The user will be redirected to the reset password page
-// The user can then enter a new password and confirm the new password
-// The user can then click on the reset password button to reset their password
-// The user will be redirected to the sign in page
-// The user can then sign in with their new password
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+Future<void> sendPasswordResetEmail({
+  required BuildContext context,
+  required String email,
+}) async {
+  final emailTrimmed = email.trim();
+
+  if (emailTrimmed.isEmpty) {
+    _showSnackBar(context, 'Please enter your email address');
+    return;
+  }
+
+  final emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+  if (!emailRegex.hasMatch(emailTrimmed)) {
+    _showSnackBar(context, 'Please enter a valid email address');
+    return;
+  }
+
+  try {
+    await FirebaseAuth.instance.sendPasswordResetEmail(email: emailTrimmed);
+    _showSnackBar(context, 'Password reset email sent!', isError: false);
+  } on FirebaseAuthException catch (e) {
+    String message;
+    switch (e.code) {
+      case 'user-not-found':
+        message = 'No user found with that email';
+        break;
+      case 'invalid-email':
+        message = 'Invalid email address';
+        break;
+      default:
+        message = 'Error sending reset email: ${e.message}';
+    }
+    _showSnackBar(context, message);
+  } catch (e) {
+    _showSnackBar(context, 'Unexpected error: ${e.toString()}');
+  }
+}
+
+void _showSnackBar(BuildContext context, String message, {bool isError = true}) {
+  ScaffoldMessenger.of(context).hideCurrentSnackBar();
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(message),
+      backgroundColor: isError ? Colors.red : Colors.green,
+      duration: const Duration(seconds: 3),
+    ),
+  );
+}
