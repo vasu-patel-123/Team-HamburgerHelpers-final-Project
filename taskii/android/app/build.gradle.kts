303cdb9e24f7033505020f5b7520bdfdccfda647
Jaron Durkee <61402697+Blarnix@users.noreply.github.com>
2025-05-05 06:39:09 -0400

    Fix build.gradle.kts to remove Flutter plugin dependency and hardcode SDK versions; streamline ndk configuration and clean up unnecessary comments.

--- taskii/android/app/build.gradle.kts
+++ taskii/android/app/build.gradle.kts
@@ -7,69 +7,45 @@ plugins {
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
-val flutter by extensions.getByName("flutter")
-ndkVersion = project.extra["flutter.ndkVersion"] as String
+
 android {
     namespace = "io.teamh.taskii"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = project.extra["flutter.ndkVersion"] as String
-    buildToolsVersion = flutter.buildToolsVersion ?: "33.0.0"
-    defaultConfig {
-        ndk {
-            checkNotNull(flutter) { "Flutter plugin extension is not configured properly." }
-            abiFilters.addAll(flutter.defaultNdkAbiFilters)
-        }
-    }
-
-        sourceCompatibility = JavaVersion.VERSION_24
-        sourceCompatibility = JavaVersion.VERSION_24
-        targetCompatibility = JavaVersion.VERSION_24
-
-    kotlinOptions {
-        // Optionally, you can add:
-        // apiVersion = "2.1"
-        // languageVersion = "2.1"
-        jvmTarget = JavaVersion.VERSION_24.toString()
-        jvmTarget = "24"
-
-    java {
-        toolchain {
-            languageVersion.set(JavaLanguageVersion.of(24))
-        }
-    }
+    compileSdk = 35 // Or use flutter.compileSdkVersion if available
+    ndkVersion = "28.1.13356709"
+    buildToolsVersion = "33.0.0"
 
     defaultConfig {
         applicationId = "io.teamh.taskii"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        targetSdk = 35 // Or use flutter.targetSdkVersion if available
+        versionCode = 1 // Or use flutter.versionCode if available
+        versionName = "1.0" // Or use flutter.versionName if available
+        ndk {
+            abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a", "x86_64"))
+        }
     }
 
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
-            // Replace this with your own signing config for the release build.
-            // Example:
-            // signingConfig = signingConfigs.create("release") {
-            //     keyAlias = "your-key-alias"
-            //     keyPassword = "your-key-password"
-            //     storeFile = file("path/to/your/keystore.jks")
-flutter {
-    // Update the source path to point to the correct Flutter project directory
-    source = "../../.."
-}
+        }
     }
+
     sourceSets["main"].java.srcDirs("src/main/kotlin")
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
-compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-}
+// Remove invalid flutter { source = ... } blocks
 
+// If you need to configure the Flutter extension, do it like this:
 flutter {
-    source = "../.."
+    // No 'source' property needed here
 }
